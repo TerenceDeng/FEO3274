5366a60ab4ef03237fc5f2220f3d12ed9754786e
pou-belle <markellazax@gmail.com>
2022-04-09 18:51:05 +0200

    Update DiscreteD.py
    @DiscreteD - the rand function

--- Assignment_1_EQ2341/PattRecClasses/DiscreteD.py
+++ Assignment_1_EQ2341/PattRecClasses/DiscreteD.py
@@ -26,8 +26,9 @@ class DiscreteD:
         """
         
         #*** Insert your own code here and remove the following error message 
+         random_values=np.random.choice(len(self.probMass),nData,p=self.probMass)
+        return random_values
         
-        print('Not yet implemented')
         
         
     def init(self, x):
