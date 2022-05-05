cf386c622f8c514d268b4071c47e6d2e6c37c275
Javier Kipen <kipen@kth.se>
2022-05-05 10:28:17 +0200

    Changes to generate the picture of outputs

--- Assignment 3/Test.py
+++ Assignment 3/Test.py
@@ -11,32 +11,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-##To test that it works with discrete distributions we use excercise 5.2 from book. Also checking infinite HMM
-
-
-mc1 = MarkovChain( np.array( [ 0.5, 0.5 ] ), np.array( [ [ 0.2, 0.8 ], [ 0.8, 0.2 ] ] ) ) 
-mc2 = MarkovChain( np.array( [ 0.5, 0.5 ] ), np.array( [ [ 0.8, 0.2 ], [ 0.2, 0.8 ] ] ) ) 
-
-D1 = DiscreteD( [0.1,0.2,0.3,0.4] )   # Distribution for state = 1
-D2 = DiscreteD( [0.4,0.3,0.2,0.1] )   # Distribution for state = 2
-
-h1  = HMM( mc1, [D1, D2])
-h2  = HMM( mc2, [D1, D2])
-
-z=np.array([[3,1,4,2]])
-
-#In order for this one to work I had to:
-    #Create prob function in discrete D.
-    #Solved dimentions notations, sometimes it was called row when in our code was a column
-alphas_1,cs_1=h1.forward(z,norm=False);
-p1=np.prod(cs_1);
-alphas_2,cs_2=h2.forward(z,norm=False);
-p2=np.prod(cs_2);
-
-
-print("For lambda1 the book says that P=" + "0.005704" + " and the obtained probability is " + str(p1))
-print("For lambda2 the book says that P=" + "0.002824" + " and the obtained probability is " + str(p2))
-
 #Since the logic is fine now, I will check that there are no problems when the distributions are others:
     
 u1=0;u2=3;std1=1;std2=2;
@@ -73,5 +47,40 @@ h  = HMM( mc, [g1, g2])                # The HMM
 x=np.array([[-0.2,2.6,1.3]])
 
 alphas,cs=h.forward(x);
-
+print("For the example in the book:")
+print("The alphas matrix when normalized gave us:")
+print(alphas)
+print("And the Cs normalized:")
+print(cs)
+alphas,cs=h.forward(x,norm=False);
+print("And then when not normalized we get:")
+print(cs)
+print("Finally the logprob:")
 print(h.logprob(x,norm=False)) #Shows that it gets the value mentioned in the book
+
+
+##To test that it works with discrete distributions we use excercise 5.2 from book. Also checking infinite HMM
+
+
+mc1 = MarkovChain( np.array( [ 0.5, 0.5 ] ), np.array( [ [ 0.2, 0.8 ], [ 0.8, 0.2 ] ] ) ) 
+mc2 = MarkovChain( np.array( [ 0.5, 0.5 ] ), np.array( [ [ 0.8, 0.2 ], [ 0.2, 0.8 ] ] ) ) 
+
+D1 = DiscreteD( [0.1,0.2,0.3,0.4] )   # Distribution for state = 1
+D2 = DiscreteD( [0.4,0.3,0.2,0.1] )   # Distribution for state = 2
+
+h1  = HMM( mc1, [D1, D2])
+h2  = HMM( mc2, [D1, D2])
+
+z=np.array([[3,1,4,2]])
+
+#In order for this one to work I had to:
+    #Create prob function in discrete D.
+    #Solved dimentions notations, sometimes it was called row when in our code was a column
+alphas_1,cs_1=h1.forward(z,norm=False);
+p1=np.prod(cs_1);
+alphas_2,cs_2=h2.forward(z,norm=False);
+p2=np.prod(cs_2);
+print("-------------------------------------------------------")
+print("To check that it works with discrete distributions and infinite HMM, we reproduced the problem 5.2 from the book and verified the results obtained: ")
+print("For lambda1 the book says that P=" + "0.005704" + " and the obtained probability is " + str(p1))
+print("For lambda2 the book says that P=" + "0.002824" + " and the obtained probability is " + str(p2))
